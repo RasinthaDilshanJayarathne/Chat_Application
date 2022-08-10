79a97445ea70e2c564c6067deea2b8a4bf9e457b
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-10 17:42:35 +0530

    Finished Client Interface

--- Clent_Side/src/Controller/ClientFormController.java
+++ Clent_Side/src/Controller/ClientFormController.java
@@ -1,4 +1,12 @@
 package Controller;
 
+import javafx.scene.layout.AnchorPane;
+
 public class ClientFormController {
+
+    public AnchorPane mainContext;
+    public AnchorPane chatListContext;
+    public AnchorPane headerContext;
+    public AnchorPane keeyBoardContext;
+    public AnchorPane chatContext;
 }
